d64ecd365f530fa9dc3da2963fb0488d2bdc443b
DRS <danri39@gmail.com>
2026-08-12 08:05:09 -0300

    Atualizações

---
+++ src/main/java/br/com/drs/radiotv_app_pro/RadiotvAppProApplication.java
@@ -0,0 +1,13 @@
+package br.com.drs.radiotv_app_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiotvAppProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiotvAppProApplication.class, args);
+    }
+
+}
