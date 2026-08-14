abbfbe513b9e71096f6f7ee2d1493a10dcde3db4
DRS <danri39@gmail.com>
2026-08-14 08:11:56 -0300

    Classe SecurityConfig

--- src/main/java/br/com/drs/radiotv_app_pro/config/SecurityConfig.java
+++ src/main/java/br/com/drs/radiotv_app_pro/config/SecurityConfig.java
@@ -50,6 +50,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/programa/**").permitAll()
                         .requestMatchers("/api/v1/ramoAtividade/**").permitAll()
                         .requestMatchers("/api/v1/configEscritorio/**").permitAll()
+                        .requestMatchers("/api/v1/ponto/**").permitAll()
+                        .requestMatchers("/api/v1/beneficio/**").permitAll()
 
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
